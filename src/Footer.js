ac436c3c021e8afd4a77f8ff2cbf01526834e1cc
Mireia Guix <mguix@users.noreply.github.com>
2019-12-04 19:08:29 +0000

    Added footer

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./footer.css";
+
+export default function Footer() {
+  return (
+    <p className="open-source">
+      <a href="https://github.com/mguix/mi-weather-react" target="blank">
+        {" "}
+        Open Source
+      </a>
+      , by Mireia Guix
+    </p>
+  );
+}
