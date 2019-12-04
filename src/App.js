5f299fe333ce1ca461bdd6057db0ae5954075e39
Mireia Guix <mguix@users.noreply.github.com>
2019-12-04 19:41:39 +0000

    created weather component

--- src/App.js
+++ src/App.js
@@ -1,85 +1,13 @@
 import React from "react";
 import Footer from "./Footer";
+import Weather from "./Weather";
 import "./App.css";
 
 function App() {
-  let weatherData = {
-    city: "Barcelona",
-    hour: "15",
-    minutes: "03",
-    day: "Monday",
-    date: "26",
-    month: "June",
-    year: "2019",
-    imgUrl: "http://openweathermap.org/img/wn/01d@2x.png"
-  };
-
   return (
     <div className="App">
       <div className="container">
-        <div className="current-forecast">
-          <div className="icons">
-            <img id="weatherIcon" src={weatherData.imgUrl} alt="#" />
-          </div>
-          <h2>{weatherData.city}</h2>
-          <p>
-            {weatherData.hour}:{weatherData.minutes} {weatherData.day}{" "}
-            {weatherData.date}, {weatherData.month} {weatherData.year}
-          </p>
-          <h1 className="temperature-container">
-            12
-            <span className="weather-unit">
-              <a href="/" className="active">
-                Cº
-              </a>{" "}
-              | <a href="/">ºF</a>
-            </span>
-          </h1>
-          <img src="illustration1.svg" width="350" alt="tree-ilustration" />
-        </div>
-        <div className="future-forecast">
-          <form>
-            <input
-              type="text"
-              placeholder="Search a city"
-              className="form-control"
-            />
-          </form>
-          <div className="row">
-            <div className="col">{weatherData.nextday}</div>
-            <div className="col">{weatherData.secondday}</div>
-            <div className="col">{weatherData.thirdday}</div>
-            <div className="col">{weatherData.fourthday}</div>
-          </div>
-          <div className="row">
-            <div className="col">
-              <img id="next-day-icon" src={weatherData.nextdayicon} alt="#" />
-            </div>
-            <div className="col">
-              <img
-                id="second-day-icon"
-                src={weatherData.seconddayicon}
-                alt="#"
-              />
-            </div>
-            <div className="col">
-              <img id="third-day-icon" src={weatherData.thirddayicon} alt="#" />
-            </div>
-            <div className="col">
-              <img
-                id="forth-day-icon"
-                src={weatherData.fourthdayicon}
-                alt="#"
-              />
-            </div>
-          </div>
-          <div className="row" id="forecast-temp">
-            <div className="col">{weatherData.nextdaytemp}</div>
-            <div className="col">{weatherData.seconddaytemp}</div>
-            <div className="col">{weatherData.thirddaytemp}</div>
-            <div className="col">{weatherData.fourthdaytemp}</div>
-          </div>
-        </div>
+        <Weather />
         <Footer />
       </div>
     </div>
